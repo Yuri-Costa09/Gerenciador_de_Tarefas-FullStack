2d7a95f79a555bd730a656ae91d7cb03b202ce49
Yuri Costa <yuricosta@MacBook-Pro-de-Yuri.local>
2025-02-14 08:16:08 -0300

    Backend Created

---
+++ backend/src/main/java/yuri_costa09/github/GerenciadorDeTarefas/GerenciadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package yuri_costa09.github.GerenciadorDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/yuri_costa09/github/GerenciadorDeTarefas/GerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package yuri_costa09.github.GerenciadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
