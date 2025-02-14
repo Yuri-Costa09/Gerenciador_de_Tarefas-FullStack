b58f3e83dbb95f991aa0d4ddcf93e6201a116dd4
Yuri-Costa09 <yurircostawork@gmail.com>
2025-02-14 08:49:07 -0300

    Entities created

---
+++ backend/src/main/java/entities/Task.java
@@ -0,0 +1,18 @@
+package entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean completed = false;
+}
---
+++ backend/src/main/java/entities/User.java
@@ -0,0 +1,23 @@
+package entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+}
